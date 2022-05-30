87fe7360f64ad96c8bbee30d0bbd09355e4f9e9c
Magdalena Struniawska <mag.sok@onet.pl>
2022-05-30 08:52:32 +0200

    first repository

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
+    
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onTapGesture {
+                selectedUser = User()
+                isShowingUser = true
+            }
+            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+                Button(user.id) { }
+            }
     }
 }
 
